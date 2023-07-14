e61c01d717ae575bfd7223810fed6a77aa90ced0
Luca Del Puppo <luca@delpuppo.net>
2023-07-14 23:20:45 +0200

    any vs unkown

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,20 @@
-const number: number = 1;
-const string: string = "Hello";
-const boolean: boolean = true;
-const array: number[] = [1, 2, 3];
-const tuple: [number, string] = [1, "Hello"];
-const literal: "yes" | "no" = "yes";
-const symbol: symbol = Symbol("symbol");
-const object: object = { a: 1, b: 2 };
+const a: any = 1;
 
-type Person = {
-  name: string;
-  age: number;
-};
+// a.b = 1;
 
-class PersonClass {
-  constructor(public name: string, public age: number) {}
+const res = a + 3;
+
+console.log(res);
+
+const b: unknown = 1;
+
+// b.b = 1;
+// 'b' is of type 'unknown'.ts(18046)
+
+// const res2 = b + 3;
+// 'b' is of type 'unknown'.ts(18046)
+
+if (typeof b === "number") {
+  const res3 = b + 3;
+  console.log(res3);
 }
-
-const person: Person = {
-  name: "Jack",
-  age: 32,
-};
-
-console.log("typeof person", typeof person);
-// console.log('typeof person', person instanceof Person);
-console.log("instanceof person", person instanceof PersonClass);
-console.log("person.constructor.name", person.constructor.name);
-
-const personClass: PersonClass = new PersonClass("Jack", 32);
-
-console.log("typeof personClass", typeof personClass);
-console.log("instanceof personClass", personClass instanceof PersonClass);
-console.log("personClass.constructor.name", personClass.constructor.name);
-
-const any: any = 1;
-const unknown: unknown = 1;
